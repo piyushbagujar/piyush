f9b7dfe36bf86301cb762b4aeb3337195d226d7f
piyushbagujar <160872176+piyushbagujar@users.noreply.github.com>
2024-06-18 14:26:56 +0530

    style.js

--- style.js
+++ style.js
@@ -99,7 +99,11 @@ function locomotiveAnimation() {
         cursor.style.left = `${e.clientX}px`;
         cursor.style.top = `${e.clientY}px`;
     });
-    Shery.makeMagnet("#nav-part2 h6, #nav-part1 svg");
+    Shery.makeMagnet("#nav-part2 h6, #nav-part1 svg" /* Element to target.*/, {
+      //Parameters are optional.
+      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+      duration: 1,
+    });
   
     var videoContainer = document.querySelector("#video-container");
     var video = document.querySelector("#video-container video")
@@ -155,7 +159,16 @@ function locomotiveAnimation() {
       // debug:true,
       config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7241195453907675},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.23,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.5,"range":[0,10]},"metaball":{"value":0.33,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
     });
+    Shery.imageMasker("#page4-flex" /* Element to target.*/, {
+      //Parameters are optional.
+      mouseFollower: true,
+      text: "Hello",
+      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+      duration: 1,
+    });
   }
+  
+  
   function flagAnimation() {
   
     document.addEventListener("mousemove", function (dets) {
@@ -215,6 +228,8 @@ function locomotiveAnimation() {
     })
   }
   
+
+
   loadingAnimation();
   cursorAnimation();
   locomotiveAnimation();
