94cbdd206e9ec240a0f62712915d4c9bfaf8b333
piyushbagujar <160872176+piyushbagujar@users.noreply.github.com>
2024-06-18 11:01:43 +0530

    style.js

--- style.js
+++ style.js
@@ -66,7 +66,7 @@ function locomotiveAnimation() {
     tl.to("#loader", {
       opacity: 0,
       duration: 0.2,
-      delay: 2.6,
+      delay: 2.9,
     });
     tl.from("#page1", {
       delay: 0.1,
