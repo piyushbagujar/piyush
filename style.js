558ac71fb9f56e001748f2d53eba33abf8f2d599
PIYUSH BADGUJAR <160872176+piyushbadgujar05@users.noreply.github.com>
2026-05-12 18:57:07 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,34 +1,31 @@
 function locomotiveAnimation() {
  gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  }, 
   getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
 
 
 
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+
 ScrollTrigger.refresh();
 
 }
@@ -94,8 +91,8 @@ ScrollTrigger.refresh();
         cursor.style.left = `${e.clientX}px`;
         cursor.style.top = `${e.clientY}px`;
     });
-    Shery.makeMagnet("#nav-part2 h6, #nav-part1 svg" /* Element to target.*/, {
-      //Parameters are optional.
+    Shery.makeMagnet("#nav-part2 h6, #nav-part1 svg" , {
+      
       ease: "cubic-bezier(0.23, 1, 0.320, 1)",
       duration: 1,
     });
@@ -154,8 +151,8 @@ ScrollTrigger.refresh();
       // debug:true,
       config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7241195453907675},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.23,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.5,"range":[0,10]},"metaball":{"value":0.33,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
     });
-    Shery.imageMasker("#page4-flex" /* Element to target.*/, {
-      //Parameters are optional.
+    Shery.imageMasker("#page4-flex" , {
+     
       mouseFollower: true,
       text: "Hello",
       ease: "cubic-bezier(0.23, 1, 0.320, 1)",
